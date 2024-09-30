1b4872c66624e88a50f9a9a8bc28ab08def7bdd0
Elahe Delavari <elahed@umich.edu>
2024-09-30 08:56:18 -0400

    minor changes

--- Assets/Scripts/PlaneCollision.cs
+++ Assets/Scripts/PlaneCollision.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class PlaneCollision : MonoBehaviour
 {
-    // Make sure both objects have 2D colliders and rigidbodies to detect collision
+    public GameManager gameManager; // Assign the GameManager in the Inspector
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("BirdObstacle"))
         {
-            // Trigger game over or any action you want
-            Debug.Log("Game Over!");
-            // You can call a GameOver function here to stop the game, show UI, etc.
+            // Trigger Game Over
+            gameManager.GameOver();
         }
     }
 }
